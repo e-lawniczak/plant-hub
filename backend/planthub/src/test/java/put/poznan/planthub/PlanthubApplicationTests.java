0413b8580f2fde69ba78c54e1009dec3db35cc14
Mielczarek-wiz <mielczarek.damian99@o2.pl>
2023-03-14 13:07:19 +0100

    Init

---
+++ backend/planthub/src/test/java/put/poznan/planthub/PlanthubApplicationTests.java
@@ -0,0 +1,13 @@
+package put.poznan.planthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
