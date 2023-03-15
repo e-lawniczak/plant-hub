93cf8538a8b10d5680859c217c58e7e04795eb24
Damian Mielczarek <80644925+Mielczarek-wiz@users.noreply.github.com>
2023-03-15 20:30:53 +0100

    Cors policy added

--- frontend/src/common/ApiRoutes.tsx
+++ frontend/src/common/ApiRoutes.tsx
@@ -1,6 +1,6 @@
 // const mainUrl = 'https://langcard.herokuapp.com/'
 // const mainUrl = 'http://localhost:3000/'
-const mainUrl = 'http://localhost:8080/'
+const mainUrl = 'http://localhost:8080'
 const rootRoute = {
     user: mainUrl + '/users',
   
